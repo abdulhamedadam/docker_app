b82d0e0d47a8ec833ec55ae18269ca5574516211
abdulhamedadam <abdulhamedzaghloul@gmail.com>
2023-11-01 12:25:13 +0200

    absulhamed

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+
+
+//init app
+const PORT =4000;
+const app = express();
+app.get('/',(req,res) => res.send('<h1>Hello ee</h1>'));
+
+
+app.listen(PORT, ()=> console.log('app is up and running on port :'+PORT));
